6280c6ae7eeed195c8b8b5e04adc2c6537121cd7
neiro <neiro.mail@gmail.com>
2017-06-03 12:43:46 +0300

    Install deps

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TrackerBot.Mixfile do
       app: :tracker_bot,
       version: "0.0.1",
       elixir: "~> 1.4",
-      name: "Elixir Base",
+      name: "Tracker Bot",
       homepage_url: "https://github.com/ne1ro/tracker_bot",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -14,12 +14,7 @@ defmodule TrackerBot.Mixfile do
       docs: [extras: ["README.md"], output: "./doc/app"],
       deps: deps(),
       aliases: aliases(),
-      dialyzer: [
-        plt_add_apps: [:mix],
-        flags: [
-          :unmatched_returns, :error_handling, :race_conditions, :underspecs
-        ]
-      ]
+      dialyzer: dialyzer()
     ]
   end
 
@@ -27,15 +22,17 @@ defmodule TrackerBot.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: ~w(erlexec effects guardsafe monadex timex)a,
-     mod: {TrackerBot, []},
-     elixirc_paths: elixirc_paths(Mix.env)]
+    [
+      mod: {TrackerBot, []},
+      extra_applications: ~w(logger)a,
+      elixirc_paths: elixirc_paths(Mix.env)
+    ]
   end
 
   # This makes sure your factory and any other modules in test/support are compiled
   # when in the test environment.
-  defp elixirc_paths(:test), do: ~w(lib web test/support)
-  defp elixirc_paths(_), do: ~w(lib web)
+  defp elixirc_paths(:test), do: ~w(lib test/support)
+  defp elixirc_paths(_), do: ~w(lib)
 
   # Dependencies can be Hex packages:
   #
@@ -48,18 +45,20 @@ defmodule TrackerBot.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+     {:distillery, "~> 1.4"},
+     {:edeliver, "~> 1.4"},
      {:erlexec, "~> 1.2.1"},
-     {:exrm, "~> 1.0.5"},
      {:effects, "~> 0.1.0"},
      {:guardsafe, "~> 0.5.0"},
      {:monadex, "~> 1.0.2"},
-     {:ok, "~> 1.0.0"},
+     {:httpoison, "~> 0.11.0"},
+     {:hackney, "~> 1.8"},
+     {:poison, "~> 2.2.0"},
+     {:slack, "~> 0.11.0"},
      {:logger_file_backend, "~> 0.0.9"},
-     {:progress_bar, "> 0.0.0"},
-     {:table_rex, "~> 0.8.0"},
      {:timex, "~> 3.0"},
-     {:credo, "~> 0.5", only: ~w(dev test)a},
-     {:dialyxir, "~> 0.5", only: :dev},
+     {:credo, "~> 0.8", only: ~w(dev test)a},
+     {:dialyxir, "~> 0.4", only: ~w(dev test)a, runtime: false},
      {:edeliver, ">= 1.2.9", only: :dev},
      {:eper, "~> 0.94.0", only: :dev},
      {:ex_machina, "~> 0.6.1", only: ~w(dev test)a},
@@ -70,12 +69,18 @@ defmodule TrackerBot.Mixfile do
    ]
   end
 
+  # Dialyzer's configuration
+  def dialyzer, do: [
+    plt_add_deps: :apps_direct,
+    flags: ~w(-Wunmatched_returns -Werror_handling -Wrace_conditions -Wunderspecs
+              -Wunknown -Woverspecs -Wspecdiffs)
+  ]
+
   defp aliases do
     [
       build: "release",
       server: "run",
-      quality: ["dialyzer", "credo --strict"],
-      ci: ~w(run_espec quality),
+      quality: ["compile --warnings-as-errors --force", "credo --strict", "dialyzer"],
       setup: [
         "local.hex --force",
         "local.rebar --force",
