6d02e0bcac8fbac2a969cff3f1a65e49612d5471
neiro <neiro.mail@gmail.com>
2017-06-03 21:23:59 +0300

    Create report by users

--- lib/tracker_bot/management.ex
+++ lib/tracker_bot/management.ex
@@ -4,22 +4,49 @@ defmodule Management do
   """
   @omitted_states ~w(accepted)
 
-  def report, do: Pivotal.list_projects |> hd |> Map.get("id") |> report
+  def report, do: Pivotal.list_projects |> List.last |> Map.get("id") |> report
   def report(project_id) do
-    project_id
-    |> Pivotal.list_stories
-    |> Enum.reject(fn(%{"current_state" => state}) -> state in @omitted_states end)
-    |> do_report
+    stories =
+      project_id
+      |> Pivotal.list_stories
+      |> Enum.reject(fn(%{"current_state" => state}) -> state in @omitted_states end)
+
+    report =
+      project_id
+      |> Pivotal.list_people
+      |> Enum.map(&(assign_stories(&1, stories)))
+      |> do_report
+
+    File.write("report.txt", report)
   end
 
-  defp story_template(%{"current_state" => state} = story), do: """
-    1) #{story["name"]}
-    *Status*: In-Progress
-    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  defp owner?(%{"owner_ids" => owner_ids}, id), do: id in owner_ids
+
+  defp assign_stories(%{"person" => %{"id" => id}} = user, stories) do
+    user
+    |> Map.get("person")
+    |> Map.put(:stories, Enum.filter(stories, &(owner?(&1, id))))
+  end
+
+  defp user_template(%{stories: stories} = user) when length(stories) > 0, do:
   """
+  #{user["name"]}
+
+  #{stories |> Enum.with_index |> Enum.map_join(&story_template/1)}
+  """
+  defp user_template(_), do: ""
+
+  defp story_template({%{"current_state" => state} = story, index}) do
+    """
+    #{index + 1}) #{String.upcase(story["name"])}
+    #{story["url"]}
+    *Status*: #{String.capitalize(story["current_state"])}
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    """
+  end
   defp story_template(_), do: ""
 
-  defp do_report(stories) when length(stories) > 0 do
+  defp do_report(people) when length(people) > 0 do
     """
     Hello everybody!
 
@@ -32,14 +59,14 @@ defmodule Management do
 
     QUESTIONS/COMMENTS:
 
-    PERSON1:
+    -
 
-    #{Enum.map_join(stories, &story_template/1)}
+    ________________________________________
+    #{Enum.map_join(people, &user_template/1)}
 
     Thanks,
-    Manager
+    Yuliana
     """
   end
-
   defp do_report(_), do: "Nothing to report 😴"
 end
