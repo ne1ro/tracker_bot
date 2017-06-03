5675a1c03fe322f82d02bb47b9ae24e3dfe66670
neiro <neiro.mail@gmail.com>
2017-06-03 15:36:39 +0300

    List projects

--- lib/pivotal.ex
+++ lib/pivotal.ex
@@ -8,12 +8,18 @@ defmodule Pivotal do
 
   @base_url "https://www.pivotaltracker.com/services/v5/"
   @token Application.fetch_env!(:tracker_bot, :pivotal_api_token)
+  @project_attributes ~w(id name)
 
-  def process_url(url), do: @base_url <> url
+  def list_projects do
+    with {:ok, %{body: projects}} <- __MODULE__.get("/projects"),
+      do: Enum.map(projects, &(Map.take(&1, @project_attributes)))
+  end
 
-  def process_response_body(body) do
+  defp process_url(url), do: @base_url <> url
+
+  defp process_response_body(body) do
     Logger.debug("Pivotal API response: #{body}")
-    Poison.decode(body)
+    Poison.decode!(body)
   end
 
   defp process_request_headers(headers), do: headers ++ ["X-TrackerToken": @token]
