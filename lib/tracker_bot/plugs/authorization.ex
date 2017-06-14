1d1bcf28e4979ceed0e71c7d5121c6dca6fd6385
neiro <neiro.mail@gmail.com>
2017-06-14 19:04:57 +0300

    Move authorization to separate plug

---
+++ lib/tracker_bot/plugs/authorization.ex
@@ -0,0 +1,21 @@
+defmodule TrackerBot.Plugs.Authorization do
+  import Plug.Conn
+
+  @allowed_users Application.fetch_env!(:tracker_bot, :allowed_users)
+  @msg "STOP RIGHT HERE CRIMINAL SCUM"
+
+  def init(opts), do: opts
+
+  def call(%{body_params: %{"message" => message}} = conn, _opts) do
+    %{"chat" => %{"id" => chat_id, "username" => username}} = message
+
+    case username in @allowed_users do
+      true ->
+        assign(conn, :chat_id, chat_id)
+      _ ->
+        Nadia.send_message(chat_id, @msg)
+        send_resp(conn, 401, @msg)
+    end
+  end
+  def call(conn, _), do: conn
+end
--- lib/tracker_bot/router.ex
+++ lib/tracker_bot/router.ex
@@ -3,37 +3,24 @@ defmodule TrackerBot.Router do
   Webhooks router
   """
   use Plug.Router
-  alias TrackerBot.Management
-
-  @allowed_users Application.fetch_env!(:tracker_bot, :allowed_users)
+  alias TrackerBot.{Plugs.Authorization, Management}
 
   if Mix.env == :dev, do: use Plug.Debugger
 
   plug :match
   plug Plug.Parsers, parsers: [:json], pass:  ["text/*", "application/*"], json_decoder: Poison
+  plug Authorization
   plug :dispatch
 
   post "/webhooks" do
-    %{"message" => %{"chat" => %{"id" => chat_id, "username" => username}, "text" => text}} = conn.body_params
-    IO.inspect(text)
-    authorize!(conn, username, chat_id)
+    %{chat_id: chat_id} = conn.assigns
+    Nadia.send_message(chat_id, "Start processing report 😫 ...")
+    Nadia.send_message(chat_id, Management.report())
+    send_resp(conn, 201, "")
   end
 
   match _ do
     send_resp(conn, 401, "Sorry, can't authorize you :(")
-  end
-
-  defp authorize!(conn, username, chat_id) do
-    case username in @allowed_users do
-      true ->
-        Nadia.send_message(chat_id, "Start processing report 😫 ...")
-        Nadia.send_message(chat_id, Management.report())
-        send_resp(conn, 201, "")
-      _ ->
-        text = "STOP RIGHT HERE CRIMINAL SCUM"
-        Nadia.send_message(chat_id, text)
-        send_resp(conn, 401, text)
-    end
   end
 
   defp help, do: """
