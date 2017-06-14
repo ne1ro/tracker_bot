9f7b4f6ddcf183d919f52a8b5d0503d686dc7136
neiro <neiro.mail@gmail.com>
2017-06-14 17:07:56 +0300

    Parse webhook body params

--- lib/tracker_bot/router.ex
+++ lib/tracker_bot/router.ex
@@ -3,22 +3,33 @@ defmodule TrackerBot.Router do
   Webhooks router
   """
   use Plug.Router
+  alias Plug.Conn
 
   if Mix.env == :dev, do: use Plug.Debugger
 
   plug :match
-  plug Plug.Parsers, parsers: [:json],
-                     pass:  ["application/json"],
+  plug Plug.Parsers, parsers: [:json], pass:  ["text/*", "application/*"],
                      json_decoder: Poison
   plug :dispatch
 
-  post "/report" do
-    {:ok, body, _conn} = read_body(conn)
-    IO.inspect(body)
-    send_resp(conn, 202, "world")
+  post "/webhooks" do
+    IO.inspect(conn.body_params)
+    %{"message" => %{"from" => %{"id" => chat_id}}} = conn.body_params
+    Nadia.send_message(chat_id, help())
+
+    send_resp(conn, 202, "")
   end
 
   match _ do
     send_resp(conn, 401, "Sorry, can't authorize you :(")
   end
+
+  defp help, do: """
+    There is no help x:D
+
+    /start - prints help
+    /help - prints help
+    /report [project name] - prints daily report
+    /projects - prints list of the projects
+  """
 end
