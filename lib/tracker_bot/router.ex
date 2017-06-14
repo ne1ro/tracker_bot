fb798a1a3980cefb99298b1c2d67df1a517e6e90
neiro <neiro.mail@gmail.com>
2017-06-14 11:39:17 +0300

    Add simple router

--- lib/tracker_bot/router.ex
+++ lib/tracker_bot/router.ex
@@ -3,4 +3,20 @@ defmodule TrackerBot.Router do
   Webhooks router
   """
   use Plug.Router
+
+  if Mix.env == :dev, do: use Plug.Debugger
+
+  plug :match
+  plug Plug.Parsers, parsers: [:json],
+                     pass:  ["application/json"],
+                     json_decoder: Poison
+  plug :dispatch
+
+  get "/hello" do
+    send_resp(conn, 200, "world")
+  end
+
+  match _ do
+    send_resp(conn, 404, "oops")
+  end
 end
