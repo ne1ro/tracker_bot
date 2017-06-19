67223363f82c68e9dc84ea24919b0afc555ab5f7
neiro <neiro.mail@gmail.com>
2017-06-19 16:45:21 +0300

    Use 4443 port

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :tracker_bot,
       cowboy_opts: [
-        port: 443,
+        port: 4443,
         otp_app: :tracker_bot,
         keyfile: "/home/deploy/.keys/bot.key",
         certfile: "/home/deploy/.keys/bot.pem"
