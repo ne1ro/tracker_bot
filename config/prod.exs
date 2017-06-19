d72d3861fc0785e1c72a39327c4c539261be038b
neiro <neiro.mail@gmail.com>
2017-06-19 16:30:47 +0300

    Fix path :(

--- config/prod.exs
+++ config/prod.exs
@@ -4,6 +4,6 @@ config :tracker_bot,
       cowboy_opts: [
         port: 443,
         otp_app: :tracker_bot,
-        keyfile: "./home/deploy/.keys/bot.key",
-        certfile: "./home/deploy/.keys/bot.pem"
+        keyfile: "/home/deploy/.keys/bot.key",
+        certfile: "/home/deploy/.keys/bot.pem"
       ]
