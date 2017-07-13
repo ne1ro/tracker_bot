5f9148103c04ff913ac221e70e111033f374846e
neiro <neiro.mail@gmail.com>
2017-07-13 13:36:49 +0300

    Show created task at report
    Fix pivotal api token

--- lib/tracker_bot/management.ex
+++ lib/tracker_bot/management.ex
@@ -6,6 +6,7 @@ defmodule TrackerBot.Management do
   alias TrackerBot.{Pivotal, Reporting}
 
   @omitted_states ~w(unstarted accepted unscheduled)
+  @ttl 2 # days
 
   def list_projects,
     do: Enum.map_join(Pivotal.list_projects, ",", &(Map.get(&1, "name")))
@@ -15,7 +16,7 @@ defmodule TrackerBot.Management do
       project_id
       |> Pivotal.list_stories
       |> Enum.reject(fn(%{"current_state" => state}) -> state in @omitted_states end)
-      |> filter_by_day
+      |> filter_by_day("updated_at", "created_at")
 
     project_id
     |> Pivotal.list_people
@@ -51,12 +52,20 @@ defmodule TrackerBot.Management do
     |> Map.put(:stories, Enum.filter(stories, &(owner?(&1, id))))
   end
 
-  defp filter_by_day(stories, field \\ "updated_at") do
-    Enum.filter(stories, fn(%{^field => time}) ->
-      time
-      |> Timex.parse!("{ISO:Extended:Z}")
-      |> Timex.compare(Timex.shift(Timex.now, days: -1)) == 1
+  defp filter_by_day(stories, field1, field2) do
+    Enum.filter(stories, fn(%{^field1 => time1, ^field2 => time2}) ->
+      compare(time1) || compare(time2)
     end)
+  end
+
+  defp filter_by_day(stories, field) do
+    Enum.filter(stories, fn(%{^field => time}) -> compare(time) end)
+  end
+
+  defp compare(time) do
+    time
+    |> Timex.parse!("{ISO:Extended:Z}")
+    |> Timex.compare(Timex.shift(Timex.now, days: -@ttl)) == 1
   end
 
   defp owner?(%{"owner_ids" => owner_ids}, id), do: id in owner_ids
